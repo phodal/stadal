2ebebc9912669c747d2ade65a9aabf198377f2d3
Phodal Huang <h@phodal.com>
2020-07-02 20:47:07 +0800

    feat: add first version memory showcase

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# stadal
+# Stadal
+
+## Documents
+
+Library:
+
+ - [heim](https://github.com/heim-rs/heim) is an ongoing attempt to create the best tool for system information fetching (ex., CPU, memory, disks or processes stats) in the Rust crates ecosystem.
+
+[Status library comparison](https://github.com/heim-rs/heim/blob/master/COMPARISON.md)
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,28 @@ mod application;
 mod domain;
 mod infrastructure;
 
-fn main() {
-    println!("Hello, world!");
+use heim::{memory, units::information, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let memory = memory::memory().await?;
+    let swap = memory::swap().await?;
+
+    println!("              total        free   available");
+    println!(
+        "{:>7} {:>11?} {:>11?} {:>11?}",
+        "Mem:",
+        memory.total().get::<information::megabyte>(),
+        memory.free().get::<information::megabyte>(),
+        memory.available().get::<information::megabyte>(),
+    );
+    println!(
+        "{:>7} {:>11?} {:>11?} {:>11?}",
+        "Swap:",
+        swap.total().get::<information::megabyte>(),
+        swap.used().get::<information::megabyte>(),
+        swap.free().get::<information::megabyte>(),
+    );
+
+    Ok(())
 }
