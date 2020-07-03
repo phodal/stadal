8991f60bc19ae660a5ae90b5fc1a5f4792d6de58
Phodal Huang <h@phodal.com>
2020-07-03 21:07:48 +0800

    feat: add rpc logic for testing

--- src/application/application.rs
+++ src/application/application.rs
@@ -1,9 +1,11 @@
-use std::sync::{Mutex, Arc, Weak, MutexGuard};
-use xi_rpc::{Handler, RpcCtx, RemoteError};
+use std::sync::{Arc, Mutex, MutexGuard, Weak};
 
+use log::{error, info, warn};
 use serde::de::{self, Deserialize, Deserializer};
 use serde::ser::{self, Serialize, Serializer};
 use serde_json::{self, Value};
+
+use xi_rpc::{Handler, RemoteError, RpcCtx, RpcPeer};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
@@ -13,16 +15,41 @@ pub enum CoreNotification {
     TracingConfig { enabled: bool },
 }
 
+pub struct Client(RpcPeer);
+
+impl Client {
+    pub fn new(peer: RpcPeer) -> Self {
+        Client(peer)
+    }
+}
+
 #[allow(dead_code)]
-pub struct CoreState {}
+pub struct CoreState {
+    peer: Client,
+}
 
 impl CoreState {
+    pub(crate) fn new(peer: &RpcPeer) -> Self {
+        CoreState {
+            peer: Client::new(peer.clone())
+        }
+    }
     pub(crate) fn client_notification(&mut self, cmd: CoreNotification) {
         use self::CoreNotification::*;
     }
 
-    fn do_new_view(&mut self) {
+    fn do_new_view(&mut self) {}
 
+    pub(crate) fn finish_setup(&mut self, self_ref: WeakStadalCore) {
+
+    }
+
+    pub(crate) fn handle_idle(&mut self, token: usize) {
+        match token {
+            _ => {
+                info!("token: {}", token);
+            }
+        }
     }
 }
 
@@ -35,7 +62,7 @@ pub enum Stadal {
 
 /// A weak reference to the main state. This is passed to plugin threads.
 #[derive(Clone)]
-pub struct WeakStadal(Weak<Mutex<CoreState>>);
+pub struct WeakStadalCore(Weak<Mutex<CoreState>>);
 
 #[allow(dead_code)]
 impl Stadal {
@@ -67,9 +94,9 @@ impl Stadal {
     }
 
     /// Returns a new reference to the core state, if core is running.
-    fn weak_self(&self) -> Option<WeakStadal> {
+    fn weak_self(&self) -> Option<WeakStadalCore> {
         match self {
-            Stadal::Running(ref inner) => Some(WeakStadal(Arc::downgrade(inner))),
+            Stadal::Running(ref inner) => Some(WeakStadalCore(Arc::downgrade(inner))),
             Stadal::Waiting => None,
         }
     }
@@ -80,10 +107,22 @@ impl Handler for Stadal {
     type Request = ();
 
     fn handle_notification(&mut self, ctx: &RpcCtx, rpc: Self::Notification) {
+        let state = CoreState::new(ctx.get_peer());
+        let state = Arc::new(Mutex::new(state));
+        *self = Stadal::Running(state);
+        let weak_self = self.weak_self().unwrap();
+        self.inner().finish_setup(weak_self);
+
         println!("handle_notification");
     }
 
     fn handle_request(&mut self, ctx: &RpcCtx, rpc: Self::Request) -> Result<Value, RemoteError> {
+        println!("!!!!!");
+        warn!("zzz");
         Ok(json!(1))
+    }
+
+    fn idle(&mut self, _ctx: &RpcCtx, token: usize) {
+        self.inner().handle_idle(token);
     }
 }
