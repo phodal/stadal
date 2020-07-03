cf9aa1b5bc73fd92d7bfc87212542aa7ebf9f9e6
Phodal Huang <h@phodal.com>
2020-07-03 17:21:55 +0800

    feat: make log files work

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,44 @@
-use std::{io, process};
+use std::{fs, io, process};
+use std::path::{Path, PathBuf};
+
 use futures::executor::block_on;
-
 use heim::{memory, Result, units::information};
-use log::error;
+use log::{error, info, warn};
 
-use xi_rpc::RpcLoop;
 use stadal::application::Stadal;
+use xi_rpc::RpcLoop;
+
+fn setup_logging(logging_path: &Path) -> Result<()> {
+    let level_filter = match std::env::var("XI_LOG") {
+        Ok(level) => match level.to_lowercase().as_ref() {
+            "trace" => log::LevelFilter::Trace,
+            "debug" => log::LevelFilter::Debug,
+            _ => log::LevelFilter::Info,
+        },
+        Err(_) => log::LevelFilter::Info,
+    };
+
+    let mut fern_dispatch = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message,
+            ))
+        })
+        .level(level_filter)
+        .chain(io::stderr());
+
+    info!("Logging with fern is set up");
+
+    fern_dispatch = fern_dispatch.chain(fern::log_file(logging_path)?);
+    fern_dispatch.apply();
+
+    info!("Writing logs to: {}", logging_path.display());
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
@@ -17,6 +50,10 @@ async fn main() {
     let memory = print_memory();
 
     block_on(memory);
+
+    if let Err(e) = setup_logging(PathBuf::from("./stadal.log").as_path()) {
+        eprintln!("[ERROR] setup_logging returned error, logging not enabled: {:?}", e);
+    }
 
     match rpc_looper.mainloop(|| stdin.lock(), &mut state) {
         Ok(_) => (),
