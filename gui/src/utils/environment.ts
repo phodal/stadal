97abb00a10edd8c9bc481e5f048670994908ee0c
Phodal Huang <h@phodal.com>
2020-07-04 23:04:46 +0800

    feat: make path works

--- gui/src/utils/environment.ts
+++ gui/src/utils/environment.ts
@@ -1,14 +1,12 @@
 import * as path from 'path';
 
-// environment
-export const DEVMODE = /node_modules[\\/]electron[\\/]/.test(process.execPath);
 export const MACOS = process.platform === 'darwin';
 export const WIN = process.platform === 'win32';
 
 // project dirs
-export const PROJECT_ROOT = path.resolve(__dirname, '..', '..');
-export const SOURCE_DIR = path.join(PROJECT_ROOT, 'src');
+const PROJECT_ROOT = path.resolve(__dirname, '..', '..');
+// const SOURCE_DIR = path.join(PROJECT_ROOT, '..');
 
 // xi-core
-export const XI_CORE_DIR = path.join(SOURCE_DIR, 'xi');
-export const XI_CORE_BIN = path.join(XI_CORE_DIR, WIN ? 'xi-core.exe' : 'xi-core');
+export const XI_CORE_DIR = path.join(PROJECT_ROOT, '../target/release/');
+export const XI_CORE_BIN = path.join(XI_CORE_DIR, WIN ? 'stadal.exe' : 'stadal');
