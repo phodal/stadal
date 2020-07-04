b1bea8a8dbba48ff6b850b1fbce343cd48bcaa21
Phodal Huang <h@phodal.com>
2020-07-04 10:59:36 +0800

    feat: import xrl source code

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,27 @@
+#[macro_use]
+extern crate log;
+extern crate log4rs;
+
+use failure::{AsFail, Error, Fail};
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(ref e) = run() {
+        use std::io::Write;
+        let stderr = &mut ::std::io::stderr();
+
+        writeln!(stderr, "error: {}", e).unwrap();
+        error!("error: {}", e);
+
+        writeln!(stderr, "caused by: {}", e.as_fail()).unwrap();
+        error!("error: {}", e);
+
+        writeln!(stderr, "backtrace: {:?}", e.backtrace()).unwrap();
+        error!("error: {}", e);
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Error> {
+    Ok(())
 }
